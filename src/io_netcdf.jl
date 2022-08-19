a55cccc87f1ca53e1f841bd2b400064db9da9209
Seth Axen <seth.axen@gmail.com>
2022-08-20 01:07:25 +0200

    Write to_netcdf docstring

--- src/io_netcdf.jl
+++ src/io_netcdf.jl
@@ -60,4 +60,31 @@ julia> close(ds);
 """
 function from_netcdf end
 
+"""
+    to_netcdf(data, dest::AbstractString; group::Symbol=:posterior, kwargs...)
+    to_netcdf(data, dest::NCDatasets.NCDataset; group::Symbol=:posterior)
+
+Write `data` to a NetCDF file.
+
+`data` is any type that can be converted to an [`InferenceData`](@ref) using
+[`convert_to_inference_data`](@ref). If not an `InferenceData`, then `group` specifies which
+group the data represents.
+
+`dest` specifies either the path to the NetCDF file or an opened NetCDF file.
+If `dest` is a path, remaining `kwargs` are passed to
+[`NCDatasets.NCDataset`](https://alexander-barth.github.io/NCDatasets.jl/stable/dataset/#NCDatasets.NCDataset).
+
+# Examples
+
+```julia
+julia> using NCDatasets
+
+julia> idata = from_namedtuple((; x = randn(4, 100, 3), z = randn(4, 100)))
+InferenceData with groups:
+  > posterior
+
+julia> to_netcdf(idata, "data.nc")
+"data.nc"
+```
+"""
 function to_netcdf end
