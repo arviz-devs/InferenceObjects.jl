08f050251be137ba14ade867d2ec7484ffcc5cae
Seth Axen <seth.axen@gmail.com>
2022-08-20 02:43:09 +0200

    Use error message syntax only on v1.8 and later

--- test/io_netcdf.jl
+++ test/io_netcdf.jl
@@ -5,9 +5,11 @@ using InferenceObjects, Test
     @testset "custom error messages are raised" begin
         path = joinpath("dummy.nc")
         @test_throws MethodError to_netcdf(idata, path)
-        @test_throws "NCDatasets is required" to_netcdf(idata, path)
         @test_throws MethodError from_netcdf(path)
-        @test_throws "NCDatasets is required" from_netcdf(path)
+        if isdefined(Base.Experimental, :register_error_hint) && VERSION ≥ v"1.8"
+            @test_throws "NCDatasets is required" to_netcdf(idata, path)
+            @test_throws "NCDatasets is required" from_netcdf(path)
+        end
     end
     include("integration/ncdatasets.jl")
 end
