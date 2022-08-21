ce4d5c7853f32341ff75e731e0f55577b3a52a83
Seth Axen <seth.axen@gmail.com>
2022-08-21 03:26:57 +0200

    Remove now unneeded dependencies

--- src/InferenceObjects.jl
+++ src/InferenceObjects.jl
@@ -3,7 +3,6 @@ module InferenceObjects
 using Dates: Dates
 using DimensionalData: DimensionalData, Dimensions, LookupArrays
 using OrderedCollections: OrderedDict
-using Requires: @require
 
 # groups that are officially listed in the schema
 const SCHEMA_GROUPS = (
@@ -28,12 +27,7 @@ const SCHEMA_GROUPS_DICT = Dict(n => i for (i, n) in enumerate(SCHEMA_GROUPS))
 const DEFAULT_SAMPLE_DIMS = Dimensions.key2dim((:chain, :draw))
 
 export Dataset, InferenceData
-export convert_to_dataset,
-    convert_to_inference_data,
-    from_namedtuple,
-    from_netcdf,
-    namedtuple_to_dataset,
-    to_netcdf
+export convert_to_dataset, convert_to_inference_data, from_namedtuple, namedtuple_to_dataset
 
 include("utils.jl")
 include("dimensions.jl")
@@ -42,20 +36,5 @@ include("inference_data.jl")
 include("convert_dataset.jl")
 include("convert_inference_data.jl")
 include("from_namedtuple.jl")
-include("io_netcdf.jl")
-
-function __init__()
-    @require NCDatasets = "85f8d34a-cbdd-5861-8df4-14fed0d494ab" include(
-        "integration/ncdatasets.jl"
-    )
-    if isdefined(Base.Experimental, :register_error_hint)
-        Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
-            if (exc.f === from_netcdf && exc.args isa Tuple{AbstractString}) ||
-                (exc.f === to_netcdf && exc.args isa Tuple{Any,AbstractString})
-                printstyled(io, "\nNCDatasets is required to use this method."; bold=true)
-            end
-        end
-    end
-end
 
 end # module
