51b66552656ccfee22271ceb8c6faa22e74baddd
Seth Axen <seth.axen@gmail.com>
2022-08-21 04:18:04 +0200

    Revert "Test subpackages using matrix"
    This reverts commit 84d14ea1b7e2f10fea97ced941811303dc8da7d1.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,12 @@
-using Pkg
-using Test
+using InferenceObjects, Test
 
-# adapted from https://github.com/SciML/Optimization.jl/blob/master/test/runtests.jl
-
-const GROUP = get(ENV, "GROUP", "InferenceObjects")
-
-function dev_subpkg(subpkg)
-    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
-    Pkg.develop(PackageSpec(path = subpkg_path))
-end
-
-function activate_subpkg_env(subpkg)
-    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
-    Pkg.activate(subpkg_path)
-    Pkg.develop(PackageSpec(path = subpkg_path))
-    Pkg.instantiate()
-end
-
-if GROUP == "InferenceObjects"
-    using InferenceObjects
-    @testset "InferenceObjects" begin
-        include("test_helpers.jl")
-        include("utils.jl")
-        include("dimensions.jl")
-        include("dataset.jl")
-        include("inference_data.jl")
-        include("convert_dataset.jl")
-        include("convert_inference_data.jl")
-        include("from_namedtuple.jl")
-    end
-else
-    dev_subpkg(GROUP)
-    subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
-    Pkg.test(PackageSpec(name = GROUP, path = subpkg_path))
+@testset "InferenceObjects" begin
+    include("test_helpers.jl")
+    include("utils.jl")
+    include("dimensions.jl")
+    include("dataset.jl")
+    include("inference_data.jl")
+    include("convert_dataset.jl")
+    include("convert_inference_data.jl")
+    include("from_namedtuple.jl")
 end
