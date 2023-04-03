99ea60e9075e25e4eaff980734c919d26c2cfd1c
Seth Axen <seth@sethaxen.com>
2023-04-03 21:09:15 +0200

    Remove workaround for type-unstable PermutedDimsArray in DimensionalData (#55)
    * Remove workaround for type-unstable PermutedDimsArray
    
    * Increment patch number

--- ext/InferenceObjectsMCMCDiagnosticToolsExt/utils.jl
+++ ext/InferenceObjectsMCMCDiagnosticToolsExt/utils.jl
@@ -5,7 +5,7 @@ function _as_marginals(data)
     sample_dims = Dimensions.dims(data, InferenceObjects.DEFAULT_SAMPLE_DIMS)
     param_dims = Dimensions.otherdims(data, sample_dims)
     dims_combined = Dimensions.combinedims(sample_dims, param_dims)
-    data_perm = _unwrappermutedims(data, dims_combined)
+    data_perm = DimensionalData.data(PermutedDimsArray(data, dims_combined))
     data_reshape = reshape(data_perm, (size(data_perm, 1), size(data_perm, 2), :))
     return data_reshape
 end
@@ -17,13 +17,4 @@ function _from_marginals(data, marginals)
     param_sizes = map(i -> size(data, i), Dimensions.dimnum(data, param_dims))
     data_reshape = reshape(marginals, param_sizes)
     return Dimensions.rebuild(data, data_reshape, param_dims)
-end
-
-# even though the permutation is const-propagated, this is not enough to make
-# PermuteDimsArray(data, perm) type-stable
-function _unwrappermutedims(da::DimensionalData.AbstractDimArray{T,N}, dims) where {T,N}
-    perm = Dimensions.dimnum(da, dims)
-    iperm = invperm(perm)
-    data = DimensionalData.data(da)
-    return PermutedDimsArray{T,N,(perm...,),(iperm...,),typeof(data)}(data)
 end
