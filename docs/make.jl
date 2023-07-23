33dd97b440f760b5228492fca278df0457b02646
Seth Axen <seth@sethaxen.com>
2023-07-23 23:59:16 +0200

    Remove final traces of InferenceObjectsNetCDF

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using InferenceObjects
-using InferenceObjectsNetCDF
 using Documenter
+using NCDatasets: NCDatasets  # load extension
 
 DocMeta.setdocmeta!(
     InferenceObjects, :DocTestSetup, :(using InferenceObjects); recursive=true
@@ -11,7 +11,7 @@ doctestfilters = [
 ]
 
 makedocs(;
-    modules=[InferenceObjects, InferenceObjectsNetCDF],
+    modules=[InferenceObjects],
     authors="Seth Axen <seth.axen@gmail.com> and contributors",
     repo="https://github.com/arviz-devs/InferenceObjects.jl/blob/{commit}{path}#{line}",
     sitename="InferenceObjects.jl",
@@ -25,7 +25,6 @@ makedocs(;
         "Home" => "index.md",
         "Dataset" => "dataset.md",
         "InferenceData" => "inference_data.md",
-        "Subpackages" => ["subpackages/inferenceobjectsnetcdf.md"],
     ],
     doctestfilters=doctestfilters,
     strict=Documenter.except(:missing_docs),
