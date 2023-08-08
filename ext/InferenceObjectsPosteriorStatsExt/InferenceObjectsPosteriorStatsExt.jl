c677a4e0f601a176b8ebbff8556d9f88c9366484
Seth Axen <seth@sethaxen.com>
2023-08-08 12:37:07 +0200

    Fixes for older Julia versions

--- ext/InferenceObjectsPosteriorStatsExt/InferenceObjectsPosteriorStatsExt.jl
+++ ext/InferenceObjectsPosteriorStatsExt/InferenceObjectsPosteriorStatsExt.jl
@@ -1,15 +1,13 @@
 module InferenceObjectsPosteriorStatsExt
 
 if isdefined(Base, :get_extension)
-    using Compat: stack
     using DimensionalData: DimensionalData, Dimensions, LookupArrays
     using InferenceObjects: InferenceObjects
     using PosteriorStats: PosteriorStats
     using StatsBase: StatsBase
 else  # using Requires
-    using ..Compat: stack
     using ..DimensionalData: DimensionalData, Dimensions, LookupArrays
-    using ..InferenceObjects: InferenceObjects
+    using ..InferenceObjects: InferenceObjects, stack
     using ..PosteriorStats: PosteriorStats
     using ..StatsBase: StatsBase
 end
--- src/InferenceObjects.jl
+++ src/InferenceObjects.jl
@@ -62,9 +62,11 @@ function __init__()
             include("../ext/InferenceObjectsNCDatasetsExt/InferenceObjectsNCDatasetsExt.jl")
         end
         @require PosteriorStats = "7f36be82-ad55-44ba-a5c0-b8b5480d7aa5" begin
-            include(
-                "../ext/InferenceObjectsPosteriorStatsExt/InferenceObjectsPosteriorStatsExt.jl",
-            )
+            @require StatsBase = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91" begin
+                include(
+                    "../ext/InferenceObjectsPosteriorStatsExt/InferenceObjectsPosteriorStatsExt.jl",
+                )
+            end
         end
     end
     if isdefined(Base.Experimental, :register_error_hint)
