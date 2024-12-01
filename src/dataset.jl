e70512b7c46ec62117517c96b44371eeaff91b16
Seth Axen <seth@sethaxen.com>
2024-12-01 22:24:22 +0100

    Remove outdated docstring

--- src/dataset.jl
+++ src/dataset.jl
@@ -7,10 +7,6 @@ This type is an
 [`DimensionalData.AbstractDimStack`](https://rafaqz.github.io/DimensionalData.jl/stable/stacks)
 that implements the same interface as `DimensionalData.DimStack` and has identical usage.
 
-When a `Dataset` is passed to Python, it is converted to an `xarray.Dataset` without copying
-the data. That is, the Python object shares the same memory as the Julia object. However,
-if an `xarray.Dataset` is passed to Julia, its data must be copied.
-
 # Constructors
 
     Dataset(data::DimensionalData.AbstractDimArray...)
